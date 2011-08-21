339929e92b6097209c254381b3a9f35dd3053416
DSB <admin@mysqldumper.de>
2011-08-21 18:42:31 +0000

    Prepared integration of unit tests

---
+++ tests/bootstrap.php
@@ -0,0 +1,50 @@
+<?php
+// set up test environment
+
+// Define path to application directory
+define('DS', DIRECTORY_SEPARATOR);
+defined('APPLICATION_PATH') || define('APPLICATION_PATH', realpath(dirname(__FILE__) . DS . '..' . DS . 'application'));
+defined('EXPORT_PATH') || define('EXPORT_PATH', realpath(APPLICATION_PATH . DS . '..' . DS . 'data' . DS . 'export'));
+defined('DOWNLOAD_PATH') || define('DOWNLOAD_PATH', realpath(EXPORT_PATH . DS . '..' . DS . 'downloads'));
+
+// Define path to application directory
+defined('APPLICATION_PATH') || define(
+    'APPLICATION_PATH', realpath(
+        dirname(__FILE__) . DS . '..' . DS . 'application')
+);
+
+// Define path to test directory
+defined('TEST_PATH') || define(
+    'TEST_PATH', realpath(dirname(__FILE__) . '/')
+);
+
+// Define application environment
+if (!defined('APPLICATION_ENV')) {
+    define('APPLICATION_ENV', 'testing');
+}
+// Ensure library/ is on include_path
+set_include_path(
+    implode(
+        PATH_SEPARATOR, array(
+            realpath(APPLICATION_PATH . '/../application'),
+            realpath(APPLICATION_PATH . '/../library'),
+            realpath(APPLICATION_PATH . '/views/helpers'),
+            get_include_path()
+        )
+    )
+);
+
+
+require_once 'Zend/Application.php';
+require_once 'PHPUnit/Autoload.php';
+require_once 'ControllerTestCase.php';
+require_once 'Testhelper.php';
+Testhelper::setUp();
+
+$application = new Zend_Application(
+    APPLICATION_ENV,
+    APPLICATION_PATH . '/configs/application.ini'
+);
+$application->bootstrap();
+clearstatcache();
+

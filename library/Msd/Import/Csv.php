e88f2bead1dc07612a1a902cfb918d47b7ae3e22
sthartmann <hartmann.steffen@gmx.net>
2011-08-24 20:22:35 +0000

    Added generic CSV importer

---
+++ library/Msd/Import/Csv.php
@@ -0,0 +1,50 @@
+<?php
+/*
+ * Generic comma seperated file importer
+ */
+
+class Msd_Import_Csv implements Msd_Import_Interface {
+
+    /**
+     * @var string
+     */
+    private $_data;
+
+    /*
+     * @var array
+     */
+    private $_lines;
+
+    /*
+     * @var array
+     */
+    private $_currentLine;
+
+    /**
+     * Will hold detected and extracted data
+     * @var array
+     */
+    protected $_extractedData = array();
+
+    public function extract($data) {
+
+        $this->_data = $data;
+        unset($data);
+        $this->_extractedData = array();
+
+        $this->_lines = explode("\n",$this->_data);
+
+        for ($i = 0; $i < count($this->_lines); $i++) {
+
+            $this->_currentLine = explode(";",$this->_lines[$i]);
+            $this->_extractedData[$this->_currentLine[0]] = $this->_currentLine[1];
+
+        }
+
+        return $this->_extractedData;
+
+    }
+
+}
+
+?>

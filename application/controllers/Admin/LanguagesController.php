37945408b8f8ef9f59f509fe78e002e4478663a0
DSB <admin@mysqldumper.de>
2011-09-03 12:58:04 +0000

    QA

--- application/controllers/Admin/LanguagesController.php
+++ application/controllers/Admin/LanguagesController.php
@@ -41,25 +41,25 @@ class Admin_LanguagesController extends AdminController
     public function editAction()
     {
         $this->view->inputErrors = array();
-        $id = $this->_request->getParam('id', 0);
+        $id          = $this->_request->getParam('id', 0);
         $intValidate = new Zend_Validate_Int();
         if (!$intValidate->isValid($id)) {
             $this->_response->setRedirect(Zend_Controller_Front::getInstance()->getBaseUrl());
         }
         $this->view->langId = $id;
-        $this->view->flag = $this->_request->getParam('flag', false);
+        $this->view->flag   = $this->_request->getParam('flag', false);
         if ($this->_request->isPost()) {
-            $langLocale = $this->_request->getParam('langLocale');
-            $langActive = $this->_request->getParam('langActive', 0);
-            $langName = $this->_request->getParam('langName');
+            $langLocale   = $this->_request->getParam('langLocale');
+            $langActive   = $this->_request->getParam('langActive', 0);
+            $langName     = $this->_request->getParam('langName');
             $flagUploaded = array_key_exists('langFlag', $_FILES) && ($_FILES['langFlag']['size'] > 0);
-            $sourceExt = $this->_request->getParam('flagExt');
+            $sourceExt    = $this->_request->getParam('flagExt');
             $upload = null;
             if ($flagUploaded) {
                 $upload = new Zend_File_Transfer_Adapter_Http();
                 $sourceFile = $upload->getFileName();
                 if (is_string($sourceFile)) {
-                    $sourceExt = pathinfo($sourceFile, PATHINFO_EXTENSION);
+                    $sourceExt  = pathinfo($sourceFile, PATHINFO_EXTENSION);
                     $targetFile = realpath(APPLICATION_PATH . '/../public/images/flags') . "/$langLocale.$sourceExt";
                     $upload->addFilter('Rename', array('target' => $targetFile, 'overwrite' => true));
                 }
@@ -79,16 +79,16 @@ class Admin_LanguagesController extends AdminController
                 }
                 $this->view->creationResult = $creationResult;
             }
-            $this->view->langActive = $langActive;
-            $this->view->langLocale = $langLocale;
-            $this->view->langName = $langName;
+            $this->view->langActive    = $langActive;
+            $this->view->langLocale    = $langLocale;
+            $this->view->langName      = $langName;
             $this->view->flagExtension = $sourceExt;
         } else {
             $langData = $this->_languagesModel->getLanguageById($id);
             if (count($langData) > 0) {
-                $this->view->langActive = $langData['active'];
-                $this->view->langLocale = $langData['locale'];
-                $this->view->langName = $langData['name'];
+                $this->view->langActive    = $langData['active'];
+                $this->view->langLocale    = $langData['locale'];
+                $this->view->langName      = $langData['name'];
                 $this->view->flagExtension = $langData['flag_extension'];
             }
         }
@@ -102,14 +102,14 @@ class Admin_LanguagesController extends AdminController
      */
     public function deleteFlagAction()
     {
-        $id = $this->_request->getParam('id', 0);
+        $id          = $this->_request->getParam('id', 0);
         $intValidate = new Zend_Validate_Int();
         if (!$intValidate->isValid($id)) {
             $this->_response->setRedirect(Zend_Controller_Front::getInstance()->getBaseUrl());
         }
-        $lang = $this->_languagesModel->getLanguageById($id);
+        $lang         = $this->_languagesModel->getLanguageById($id);
         $deleteResult = 'deleted';
-        $imageFile = realpath(APPLICATION_PATH . '/../public/images/flags')
+        $imageFile    = realpath(APPLICATION_PATH . '/../public/images/flags')
             . "/{$lang['locale']}.{$lang['flag_extension']}";
         if (!@unlink($imageFile)) {
             $deleteResult = 'notDeleted';
@@ -138,14 +138,14 @@ class Admin_LanguagesController extends AdminController
         Zend_File_Transfer_Adapter_Abstract $flag = null
     )
     {
-        $inputsValid = true;
-        $intValidate = new Zend_Validate_Int();
-        $inputsValid &= $intValidate->isValid($id);
+        $inputsValid     = true;
+        $intValidate     = new Zend_Validate_Int();
+        $inputsValid    &= $intValidate->isValid($id);
         $betweenValidate = new Zend_Validate_Between(array('min' => 0, 'max' => 1));
-        $inputsValid &= $intValidate->isValid($active);
-        $inputsValid &= $betweenValidate->isValid($active);
-        $strLenValidate = new Zend_Validate_StringLength(array('min' => 2, 'max' => 5));
-        $inputErrors = array();
+        $inputsValid    &= $intValidate->isValid($active);
+        $inputsValid    &= $betweenValidate->isValid($active);
+        $strLenValidate  = new Zend_Validate_StringLength(array('min' => 2, 'max' => 5));
+        $inputErrors     = array();
         $langLocaleValid = $strLenValidate->isValid($langLocale);
         if (!$langLocaleValid) {
             $inputErrors['langLocale'] = $strLenValidate->getMessages();

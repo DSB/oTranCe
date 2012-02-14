315102aafea31840fa7861b2be53c7495707b177
DSB <admin@mysqldumper.de>
2012-02-14 23:40:45 +0000

    Added view helper for escaping javascript messages

---
+++ application/views/helpers/JsEscape.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * This file is part of MySQLDumper released under the GNU/GPL 2 license
+ * http://www.mysqldumper.net
+ *
+ * @package         MySQLDumper
+ * @subpackage      View_Helpers
+ * @version         SVN: $Rev$
+ * @author          $Author$
+ */
+
+/**
+ * Escape Javascript output
+ *
+ * @package         MySQLDumper
+ * @subpackage      View_Helpers
+ */
+class Msd_View_Helper_JsEscape extends Zend_View_Helper_Abstract
+{
+    /**
+     * Escape ' and " for use in Javascript statements
+     *
+     * @param string $text The text to escape
+     *
+     * @return string
+     */
+    public function jsEscape($text)
+    {
+        $text = str_replace('"', '\"', $text);
+        $text = str_replace("'", "\'", $text);
+        return $text;
+    }
+
+}

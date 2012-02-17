4417b1bcf5c1324d8340f6a5230307961dbcf1af
DSB <admin@mysqldumper.de>
2012-02-17 18:36:15 +0000

    OTC-97 Added a new view helpers/PrintFileTemplateHtml.php: it decides whether to show a hidden input (if we only have one file template) or a selectbox (if we have more than 1)
    Entries / Add a key + Entries /Edit a key:
    - use helper to output text or selctbox
    
    CSS:
    - some changes for layout

---
+++ application/views/helpers/PrintFileTemplateHtml.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * This file is part of MySQLDumper released under the GNU/GPL 2 license
+ * http://www.mysqldumper.net
+ *
+ * @package         MySQLDumper
+ * @subpackage      View_Helpers
+ * @version         SVN: $Rev$
+ * @author          $Author$
+ */
+
+/**
+ * Print file template as selectbox or hidden input
+ *
+ * @package         MySQLDumper
+ * @subpackage      View_Helpers
+ */
+class Msd_View_Helper_PrintFileTemplateHtml extends Zend_View_Helper_Abstract
+{
+    /**
+     * Holds filetemplates id and name
+     *
+     * @var array
+     */
+    private static $_fileTemplates;
+
+    /**
+     * Return list of file templates as selectbox or, if there is only one configured, as hidden input.
+     *
+     * @param int $selFileTemplateId ID of selected file template
+     *
+     * @return string
+     */
+    public function printFileTemplateHtml($selFileTemplateId)
+    {
+        if (self::$_fileTemplates === null) {
+            $templatesModel = new Application_Model_FileTemplates();
+            $fileTemplates = $templatesModel->getFileTemplates();
+            foreach ($fileTemplates as $template) {
+                self::$_fileTemplates[$template['id']] = $template;
+            }
+        }
+        $ret = '';
+        if (sizeof(self::$_fileTemplates) > 1) {
+            $ret .= '<select class="select" name="fileTemplate" style="width:402px;">';
+            foreach (self::$_fileTemplates as $fileTemplate) {
+                $ret .= '<option value="' . $fileTemplate['id'] . '"';
+                if ($selFileTemplateId == $fileTemplate['id']) {
+                    $ret .= ' selected="selected"';
+                }
+                $ret .= '>' . $fileTemplate['filename'] . '</option>';
+            }
+            $ret .= '</select>';
+        } elseif (sizeof(self::$_fileTemplates) == 1) {
+            $values = array_values(self::$_fileTemplates);
+            $ret .= '<input type="hidden" name="fileTemplate" value="' . $values[0]['id'] .'" />'
+                    . $values[0]['filename'];
+
+        } else {
+            $ret = "xx";
+        }
+
+        return $ret;
+    }
+}

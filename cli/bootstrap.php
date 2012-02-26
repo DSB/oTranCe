c9c1e1a98fd48e0bb5b0b160a8b6946ad702cae5
DSB <admin@mysqldumper.de>
2012-02-26 21:16:03 +0000

    QA

--- cli/bootstrap.php
+++ cli/bootstrap.php
@@ -8,10 +8,7 @@ defined('EXPORT_PATH') || define('EXPORT_PATH', realpath(APPLICATION_PATH . DS .
 defined('DOWNLOAD_PATH') || define('DOWNLOAD_PATH', realpath(EXPORT_PATH . DS . '..' . DS . 'downloads'));
 
 // Define path to application directory
-defined('APPLICATION_PATH') || define(
-    'APPLICATION_PATH', realpath(
-        dirname(__FILE__) . DS . '..' . DS . 'application')
-);
+defined('APPLICATION_PATH') || define('APPLICATION_PATH', realpath(dirname(__FILE__) . DS . '..' . DS . 'application'));
 
 // Ensure library/ is on include_path
 set_include_path(

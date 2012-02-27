cb755dba1cfdc33ac1de200daad57482351f23f1
DSB <admin@mysqldumper.de>
2012-02-27 22:26:23 +0000

    QA: - added a test for view helper CutString

---
+++ tests/unit/application/views/helpers/CutStringTest.php
@@ -0,0 +1,19 @@
+<?php
+require_once 'PHPUnit/Framework/TestCase.php';
+require_once 'CutString.php';
+
+/**
+ * @group MsdViewHelper
+ */
+
+class CutStringTest extends PHPUnit_Framework_TestCase
+{
+    public function testCutStringSavesWords()
+    {
+        $expected='I will';
+        $viewHelper = new Msd_View_Helper_CutString();
+        $res = $viewHelper->cutString('I will be cut off', 3);
+        $this->assertEquals($expected, $res);
+    }
+}
+

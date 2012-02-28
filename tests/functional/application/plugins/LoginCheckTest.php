40e1ad991bf31f6a9f760ca2a80600f317a77fba
DSB <admin@mysqldumper.de>
2012-02-28 23:14:23 +0000

    QA - fixed test

--- tests/functional/application/plugins/LoginCheckTest.php
+++ tests/functional/application/plugins/LoginCheckTest.php
@@ -14,7 +14,7 @@ class LoginCheckTest extends ControllerTestCase
     public function testDoesNotRedirectIfUserIsLoggedIn()
     {
         $this->loginUser();
-        $this->dispatch('settings/index');
+        $this->dispatch('/settings/index');
         $this->assertResponseCode('200');
         $this->assertNotRedirect();
         $this->assertController('settings');
--- tests/functional/application/views/helpers/GetUsernameTest.php
+++ tests/functional/application/views/helpers/GetUsernameTest.php
@@ -1,4 +1,5 @@
 <?php
+require_once 'GetUsername.php';
 /**
  * @group MsdViewHelper
  */

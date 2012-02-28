bcfafa6a279b6be55b0f0bc9713a68e890361648
DSB <admin@mysqldumper.de>
2012-02-28 21:49:50 +0000

    QA - added test for helper PrintStatusIcon

---
+++ tests/unit/application/views/helpers/PrintStatusIconTest.php
@@ -0,0 +1,40 @@
+<?php
+require_once 'PHPUnit/Framework/TestCase.php';
+require_once 'PrintStatusIcon.php';
+
+/**
+ * @group MsdViewHelper
+ */
+
+class PrintStatusIconTest extends PHPUnit_Framework_TestCase
+{
+    public function testCanPrintStatusIcon()
+    {
+        $this->view = Zend_Layout::getMvcInstance()->getView();
+        $expected='<img src="/css/otc/icons/16x16/NotOk.png" alt="" title=""/>';
+        $res = $this->view->printStatusIcon(false);
+        $this->assertEquals($expected, $res);
+
+        $expected='<img src="/css/otc/icons/16x16/Apply.png" alt="" title=""/>';
+        $res = $this->view->printStatusIcon(true);
+        $this->assertEquals($expected, $res);
+    }
+
+    public function testCanPrintStatusIconWithTitle()
+    {
+        $this->view = Zend_Layout::getMvcInstance()->getView();
+        $expected='<img src="/css/otc/icons/16x16/NotOk.png" alt="testTitle" title="testTitle"/>';
+        $res = $this->view->printStatusIcon(false, 'testTitle');
+        $this->assertEquals($expected, $res);
+    }
+
+    public function testCanPrintStatusIconWithClass()
+    {
+        $this->view = Zend_Layout::getMvcInstance()->getView();
+        $expected='<img src="/css/otc/icons/16x16/NotOk.png" alt="testTitle" title="testTitle" class="myClass"/>';
+        $res = $this->view->printStatusIcon(false, 'testTitle', 'myClass');
+        $this->assertEquals($expected, $res);
+    }
+
+}
+

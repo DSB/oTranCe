78b126e770ecf0edc0c9b55db5570b6da4647176
DSB <admin@mysqldumper.de>
2012-02-28 21:33:09 +0000

    QA - test added

--- tests/functional/application/views/helpers/PrintFlagTest.php
+++ tests/functional/application/views/helpers/PrintFlagTest.php
@@ -17,4 +17,17 @@ class PrintFlagTest extends ControllerTestCase
         $this->assertEquals('<img class="flag" src="/images/flags/en.gif" alt="English" title="English"/>', $res);
     }
 
+    public function testCanPrintFlagWithWidthSet()
+    {
+        $this->view = Zend_Layout::getMvcInstance()->getView();
+        $res = $this->view->printFlag(1, 20);
+        $this->assertEquals('<img class="flag" src="/images/flags/de.gif" alt="Deutsch" title="Deutsch" width="20"/>', $res);
+    }
+
+    public function testCanPrintFlagWithIdSet()
+    {
+        $this->view = Zend_Layout::getMvcInstance()->getView();
+        $res = $this->view->printFlag(1, 20, 'myId');
+        $this->assertEquals('<img class="flag" src="/images/flags/de.gif" alt="Deutsch" title="Deutsch" width="20" id="myId"/>', $res);
+    }
 }

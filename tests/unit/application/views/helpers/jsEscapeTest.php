e9a007b8cc4102e49703e0515d2b8d31e2ea5795
DSB <admin@mysqldumper.de>
2012-02-28 20:34:44 +0000

    QA: - unit test added

---
+++ tests/unit/application/views/helpers/jsEscapeTest.php
@@ -0,0 +1,20 @@
+<?php
+require_once 'PHPUnit/Framework/TestCase.php';
+require_once 'JsEscape.php';
+
+/**
+ * @group MsdViewHelper
+ */
+
+class JsEscapeTest extends PHPUnit_Framework_TestCase
+{
+    public function testCanEscape()
+    {
+        $expected= 'I\\\'m a message with \\\'quotes\\\' and \"double quotes\" in it.';
+        $viewHelper = new Msd_View_Helper_JsEscape();
+        $res = $viewHelper->jsEscape('I\'m a message with \'quotes\' and "double quotes\" in it.');
+        $this->assertEquals($expected, $res);
+    }
+
+}
+

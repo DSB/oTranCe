baeafc6702a8856d03f8a1f5f390dd77eab587a3
DSB <admin@mysqldumper.de>
2012-02-29 22:01:35 +0000

    QA - added base test for statistics model

---
+++ tests/functional/application/models/StatisticsTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @group Statistics
+ */
+class StatisticsTest extends ControllerTestCase
+{
+    public function testGetUserstatistics()
+    {
+        /**
+         * @var Application_Model_Statistics
+         */
+        $this->statisticsModel = new Application_Model_Statistics();
+        $statistics = $this->statisticsModel->getUserstatistics();
+        //@TODO create fixture with statistic data for test
+        $this->assertEquals(array(), $statistics);
+    }
+
+}

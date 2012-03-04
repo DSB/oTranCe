2c76fecd572e5e58dd17dca8d8243c70f075b7ac
DSB <admin@mysqldumper.de>
2012-03-04 17:35:44 +0000

    QA - added test for index controller

---
+++ tests/functional/application/controllers/IndexControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @group Controllers
+ */
+class IndexControllerTest extends ControllerTestCase
+{
+    public function testLogoutAction()
+    {
+        $this->loginUser();
+        $this->dispatch('/index/logout');
+        $this->resetResponse();  // clear header from cookie
+        // request a page which needs a log in
+        $this->dispatch('/settings');
+        // we must be redirected to the log in page
+        $this->assertRedirect('/index/login');
+    }
+}

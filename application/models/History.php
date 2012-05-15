2cdffe9394842ac47d3a7b42a8c1928b3e3ad9ca
DSB <admin@mysqldumper.de>
2012-05-15 18:28:45 +0000

    QA

--- application/models/History.php
+++ application/models/History.php
@@ -83,8 +83,8 @@ class Application_Model_History extends Msd_Application_Model
      * Compare old with new values and log changes
      *
      * @param string $keyId     Id of key
-     * @param array  $oldValues Old translation
-     * @param array  $newValues New translation
+     * @param array  $oldValues Old translations
+     * @param array  $newValues New translations
      *
      * @return void
      */

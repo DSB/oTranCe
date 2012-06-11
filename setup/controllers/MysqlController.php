4d1cf2c55de494e2667075bf1713d33adc01ce02
D4rk4ng3l <stefan-krenz@mayflower.de>
2012-06-11 21:47:42 +0000

    OTC-137 - Improved setup build script. - Added check for the existence of the database. - Added database clean up if it exists.

--- setup/controllers/MysqlController.php
+++ setup/controllers/MysqlController.php
@@ -30,19 +30,55 @@ class MysqlController extends Setup_Controller_Abstract
             $mysql['host'],
             $mysql['user'],
             $mysql['pass'],
-            $mysql['db'],
+            "",
             $mysql['port'],
             $mysql['socket']
         );
         $success = ($mysqli->connect_error === null);
-        if ($success) {
-            $_SESSION['mysql'] = $mysql;
+        if (!$success) {
+            $this->_response->setBodyJson(
+                array(
+                    'connect' => $success,
+                    'message' => $mysqli->connect_error,
+                    'number'  => $mysqli->connect_errno,
+                )
+            );
+            return;
         }
+
+        $_SESSION['mysql'] = $mysql;
+
+        $stmt = $mysqli->stmt_init();
+        $stmt->prepare('SELECT COUNT(*) FROM information_schema.SCHEMATA WHERE SCHEMA_NAME = ?');
+        $stmt->bind_param("s", $mysql['db']);
+        $stmt->execute();
+        $stmt->bind_result($row);
+        $stmt->fetch();
+        $stmt->free_result();
+
+        if ($row > 0) {
+            $this->_response->setBodyJson(
+                array(
+                    'connect' => true,
+                    'dbExists' => true,
+                    'message' => 'The database ' . $mysql['db']
+                        . ' already exists.<br/>If you continue, all data in the selected database is lost.',
+                )
+            );
+
+            return;
+        }
+
+        $success = $mysqli->query(
+            "CREATE DATABASE " . $mysql['db'] . " DEFAULT CHARSET utf8 COLLATE utf8_general_ci"
+        );
+
         $this->_response->setBodyJson(
             array(
-                'connect' => $success,
-                'message' => $mysqli->connect_error,
-                'number'  => $mysqli->connect_errno,
+                'connect' => true,
+                'dbExists' => false,
+                'dbCreate' => $success,
+                'message' => $mysqli->error,
             )
         );
     }
@@ -65,6 +101,20 @@ class MysqlController extends Setup_Controller_Abstract
         );
 
         $mysqli->query('SET NAMES utf8');
+
+        $stmt = $mysqli->stmt_init();
+        $stmt->prepare('SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_SCHEMA = ?');
+        $stmt->bind_param("s", $mysql['db']);
+        $stmt->execute();
+        $stmt->bind_result($row);
+
+        $tables = array();
+        while ($stmt->fetch()) {
+            $tables[] = $row;
+        }
+        $stmt->free_result();
+
+        $mysqli->query('DROP TABLE `' . implode('`, `', $tables) . '`');
 
         $setupSqlFile = file_get_contents($this->_config['extractDir'] . '/docs/setup.sql');
         $sqlQueries = explode(";\n", $setupSqlFile);

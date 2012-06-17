3b04d0509074bf3974ff5d3aa8ff5e2815ab01e8
D4rk4ng3l <stefan-krenz@mayflower.de>
2012-06-17 15:20:59 +0000

    Added version to footer.

---
+++ library/Msd/Version.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file is part of MySQLDumper released under the GNU/GPL 2 license
+ * http://www.mysqldumper.net
+ *
+ * @package         MySQLDumper
+ * @subpackage      Version
+ * @version         SVN: $Rev$
+ * @author          $Author$
+ */
+/**
+ * Factory class for Version Control System support.
+ *
+ * @package         MySQLDumper
+ * @subpackage      Version
+ */
+class Msd_Version
+{
+    const VERSION = '1.0.0RC1';
+}

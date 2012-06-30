b5f7f25066fc74a7963a529cc600d3c542670d32
D4rk4ng3l <stefan-krenz@mayflower.de>
2012-06-30 20:40:13 +0000

    OTC-157 - Directory structure check inplemented. - Merged style.css and login.css to setup.css

--- setup/controllers/IndexController.php
+++ setup/controllers/IndexController.php
@@ -31,9 +31,11 @@ class IndexController extends Setup_Controller_Abstract
         $this->view->rootIsWritable   = is_writable($installationRoot);
         $this->view->phpVersion       = PHP_VERSION;
         $this->view->phpVersionOk     = (version_compare(PHP_VERSION, '5.2.10') >= 0);
+        $this->view->correctPaths     = (basename(dirname(APPLICATION_PATH)) == 'public');
         $this->view->readyToInstall   = $this->view->hasJsonExtension &&
             $this->view->hasCurlExtension &&
             $this->view->rootIsWritable &&
-            $this->view->phpVersionOk;
+            $this->view->phpVersionOk &&
+            $this->view->correctPaths;
     }
 }

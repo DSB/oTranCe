66c3c345d90c240b2f82364e43a3055fd58083ad
DSB <admin@mysqldumper.de>
2012-09-21 10:00:33 +0000

    Added test for statistics model

--- tests/functional/application/models/StatisticsTest.php
+++ tests/functional/application/models/StatisticsTest.php
@@ -5,28 +5,47 @@
  */
 class StatisticsTest extends ControllerTestCase
 {
+    /**
+     * @var \Application_Model_Statistics
+     */
+    private $statisticsModel;
+
+    public function setUp()
+    {
+        $this->statisticsModel = new Application_Model_Statistics();
+    }
+
     public function testGetUserstatistics()
     {
-        /**
-         * @var Application_Model_Statistics
-         */
-        $this->statisticsModel = new Application_Model_Statistics();
         $statistics = $this->statisticsModel->getUserstatistics();
-        $expected = array(
+        $expected   = array(
             0 => array(
-                    'user_id'     => '1',
-                    'lang_id'     => '1',
-                    'editActions' => '1',
-                    'username'    => 'Admin'
-                ),
+                'user_id'     => '1',
+                'lang_id'     => '1',
+                'editActions' => '1',
+                'username'    => 'Admin'
+            ),
             1 => array(
-                    'user_id'     => '1',
-                    'lang_id'     => '2',
-                    'editActions' => '1',
-                    'username'    => 'Admin'
-                )
+                'user_id'     => '1',
+                'lang_id'     => '2',
+                'editActions' => '1',
+                'username'    => 'Admin'
+            )
         );
         $this->assertEquals($expected, $statistics);
     }
 
+    public function testCanGetUserChangeStatistics()
+    {
+        $res      = $this->statisticsModel->getUserChangeStatistics();
+        $expected = array(
+            1 =>
+            array(
+                'user_id'     => '1',
+                'editActions' => '2',
+                'lastAction'  => '2012-03-03 20:39:16',
+            ),
+        );
+        $this->assertEquals($expected, $res);
+    }
 }

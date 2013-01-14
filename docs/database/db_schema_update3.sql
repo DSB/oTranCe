5817079ae4f76f0c27fa92a2d64baf4f3289281c
Niko Kostopoulos <me@enkay.de>
2013-01-14 20:55:10 +0100

    Add SQL-Snippet for creating forgotpassword db-table Edit config.dist.ini for default configuration

---
+++ docs/database/db_schema_update3.sql
@@ -0,0 +1,9 @@
+-- Add new table for forgotpassword feature
+CREATE TABLE `forgotpasswords` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`userid` INT(11) NULL DEFAULT '0',
+	`timestamp` DATETIME NULL DEFAULT NULL,
+	PRIMARY KEY (`id`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB;

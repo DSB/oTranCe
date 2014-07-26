8bdbad39f5f06bbe367a8caf9c95bc6f61d1e28c
Daniel Schlichtholz <admin@mysqldumper.de>
2014-07-26 12:59:36 +0200

    Update for language packs

--- application/language/de/lang.php
+++ application/language/de/lang.php
@@ -9,10 +9,10 @@
  * @author        $Author: $
  */
 // The following translators have been busy on translating this language for you:
-// DSB (420), enkay (14), janrei (33), KatjaFlinzner (11), kyoya (21), Marco.Steinhäuser (79)
+// DSB (421), enkay (14), janrei (33), KatjaFlinzner (11), kyoya (21), Marco.Steinhäuser (79)
 
 $lang = array(
-'L_ACCOUNT_ACTIVATED' => 'Ihr Benutzeraccount %1$s für das Projekt %2$s wurde soeben durch einen Administrator aktiviert.',
+'L_ACCOUNT_ACTIVATED' => 'X Ihr Benutzeraccount %1$s für das Projekt %2$s wurde soeben durch einen Administrator aktiviert.',
 'L_ACCOUNT_ACTIVATED_SUBJECT' => 'Benutzeraccount %1$s für %2$s aktiviert',
 'L_ACCOUNT_OF_USER_WITH_ID' => 'Bearbeite Benutzerkonto mit der Id %s',
 'L_ACCOUNT_SETTINGS' => 'Einstellungen des Benutzerkontos',

45bc66b93db36088b7e77c816088b34bc3e4ed96
dsb <admin@mysqldumper.de>
2015-07-05 16:09:47 +0200

    Removed unmaintained languages. Added flag for language tr.

--- application/language/ar/MyMemory.php
+++
@@ -1,9 +0,0 @@
-<?php
-$lang = array(
-'L_MYMEMORY_SERVICE_ACCOUNT_DESCRIPTION' => 'If you do have a personal MyMemory account, provide your API key here. This way requests from oTranCe can be assigned to it.',
-'L_MYMEMORY_SERVICE_DESCRIPTION' => 'http://mymemory.translated.net is a free translation service. 
-Through its free web API, up to 100 requests per day can be sent.',
-'L_MYMEMORY_SERVICE_EMAIL_DESCRIPTION' => 'If you enter your contact e-mail address, it will be transmitted with each translation request to MyMemory. 
-This raises the limit to 1000 requests per day and secondly allows MyMemory, to contact you, if technical problems are discovered.',
-);
-return $lang;
